ef0b0aaa4605ee5b01f68d5216f6008a42186f0e
Leonardo Santiago <leonardo@mixrank.com>
2024-05-06 22:53:07 -0300

    remove forwardX11

--- users/leonardo.nix
+++ users/leonardo.nix
@@ -81,10 +81,7 @@
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     };
-    programs.ssh = {
-      startAgent = true;
-      forwardX11 = true;
-    };
+    programs.ssh.startAgent = true;
     services.pipewire = {
       enable = true;
       extraConfig.pipewire = {
